27ab8650d478cd55714db56d45b52f5bfc988b7f
Ho Lyun Jeong <holyun.lucas.jeong@gmail.com>
2020-12-20 21:14:53 -0800

    Added animations to trees and removed temporary buttons

--- script.js
+++ script.js
@@ -74,15 +74,30 @@ const showText = () => {
         letters[i].style.animationIterationCount = 1 + ", infinite";
     }
 };
-textBtn.addEventListener("click", showText);
+
+//Interaction counting
+const nInteractions = 5;
+var interactions = 0;
+
+const registerInteraction = () => {
+    ++interactions;
+    if (interactions == nInteractions) {
+        showText();
+    }
+}
 
 //Tree lights
-const nLights = 16;
-const lightColors = [["#7a1907", "#e03212"], ["#5e5e00", "#ebeb02"], ["#003b08", "#00e81f"], ["#003847", "#00addb"]];
-const lightRadii = [0.7, 1]
-const lightSizeRange = [4, 6];
+const nLights = 12;
+const lightOffColors = ["#7a1907", "#5e5e00", "#003b08", "#003847"];
+const lightOnColors = [[0xe0, 0x32, 0x12], [0xeb, 0xeb, 0x02], [0x00, 0xe8, 0x1f], [0x00, 0x1d, 0xdb]];
+const lightRadii = [0.7, 1];
+const lightSizeRange = [3, 5];
 const trees = document.getElementsByClassName("tree");
 var lights = [];
+var lightColors = [];
+var lightTimeline = [];
+var lightFrame = [];
+var lightDuration = [];
 
 const randomFloat = (min, max) => {
     return Math.random() * (max - min) + min;
@@ -92,82 +107,131 @@ const randomInt = (min, max) => {
     return Math.floor(randomFloat(min, max));
 };
 
+const shuffle = (array) => {
+    for (var i = 0; i < array.length - 1; ++i) {
+        const j = randomInt(i, array.length);
+        const swap = array[i];
+        array[i] = array[j];
+        array[j] = swap;
+    }
+}
+
 const checkCollision = (light1, light2) => {
-    const x1 = light1.x, y1 = light1.y, r1 = light1.size;
-    const x2 = light2.x, y2 = light2.y, r2 = light2.size;
+    const x1 = light1.style.getPropertyValue("--x") * 1, y1 = light1.style.getPropertyValue("--y") * 1, r1 = light1.style.getPropertyValue("--size") * 1;
+    const x2 = light2.style.getPropertyValue("--x") * 1, y2 = light2.style.getPropertyValue("--y") * 1, r2 = light2.style.getPropertyValue("--size") * 1;
     const dx = x2 - x1, dy = y2 - y1;
-    return dx * dx + dy * dy < (r1 + r2) * (r1 + r2);
+    return dx * dx + dy * dy < (r1 + r2) * (r1 + r2) * 1.6 * 1.6;
+}
+
+const lerp = (rgb1, rgb2, r, n) => {
+    red = Math.floor(rgb1[0] + (rgb2[0] - rgb1[0]) * r / n);
+    green = Math.floor(rgb1[1] + (rgb2[1] - rgb1[1]) * r / n);
+    blue = Math.floor(rgb1[2] + (rgb2[2] - rgb1[2]) * r / n);
+    return "rgb(" + red + ", " + green + ", " + blue + ")";
 }
 
 const createLights = () => {
     for (var i = 0; i < trees.length; ++i) {
-        lights.push([]);
-        const rect = trees[i].getBoundingClientRect();
-        const scale = rect.width / trees[i].offsetWidth;
+        const tree = trees[i];
+        const scale = tree.getBoundingClientRect().width / tree.offsetWidth;
         // ugly hard code oops
-        const leftBound = rect.left + 0.41 * rect.width;
-        const rightBound = rect.right - 0.39 * rect.width;
-        const topBound = rect.top + 0.45 * rect.height;
-        const bottomBound = rect.bottom - 0.13 * rect.height;
-        const topThreshold = rect.top + 0.47 * rect.height;
-        const topThresholdLeft = rect.left + 0.44 * rect.width;
-        const topThresholdRight = rect.right - 0.42 * rect.width;
-        for (var j = 0; j < nLights; ++j) {
-            lights[i].push({
-                x: randomFloat(leftBound, rightBound),
-                y: randomFloat(topBound, bottomBound),
-                size: scale * randomFloat(lightSizeRange[0], lightSizeRange[1]),
-                color: randomInt(0, lightColors.length),
-                lit: 0
-            });
+        const leftBound = 0.4 * tree.offsetWidth;
+        const rightBound = 0.6 * tree.offsetWidth;
+        const topBound = 0.45 * tree.offsetHeight;
+        const bottomBound = 0.87 * tree.offsetHeight;
+        const topThreshold = 0.47 * tree.offsetHeight;
+        const topThresholdLeft = 0.43 * tree.offsetWidth;
+        const topThresholdRight = 0.57 * tree.offsetWidth;
+
+        var bad = 0;
+        lights.push([]);
+        lightColors.push([]);
+        lightDuration.push([]);
+        lightTimeline.push([]);
+        lightFrame.push(0);
+        for (j = 0; j < nLights; ++j) {
+            lights[i].push(document.createElement("div"));
+            var light = lights[i][j];
+            light.className = "tree-light";
+            const size = randomFloat(lightSizeRange[0], lightSizeRange[1]);
+            light.style.setProperty("--size", size);
+            light.style.width = light.style.height = "200px";
+            light.style.setProperty("--x", randomFloat(leftBound, rightBound));
+            light.style.setProperty("--y", randomFloat(topBound, bottomBound));
 
             // resolve collision
-            while (true) {
+            var count = 0;
+            while (count < 100) {
                 var collide = false;
                 for (var k = 0; k < j; ++k) {
-                    if (checkCollision(lights[i][j], lights[i][k])) {
+                    if (checkCollision(light, lights[i][k])) {
                         collide = true;
                         break;
                     }
                 }
-                if (collide || lights[i][j].y < topThreshold && (lights[i][j].x < topThresholdLeft || lights[i][j].x > topThresholdRight)) {
-                    lights[i][j].x = randomFloat(leftBound, rightBound);
-                    lights[i][j].y = randomFloat(topBound, bottomBound);
+                if (collide || light.style.getPropertyValue("--y") < topThreshold && (light.style.getPropertyValue("--x") < topThresholdLeft || light.style.getPropertyValue("--x") > topThresholdRight)) {
+                    light.style.setProperty("--x", randomFloat(leftBound, rightBound));
+                    light.style.setProperty("--y", randomFloat(topBound, bottomBound));
                 } else {
                     break;
                 }
+                ++count;
             }
+            if (count == 100) {
+                lights.pop();
+                lights.push([]);
+                j = -1;
+                continue;
+            }
+            light.style.top = light.style.getPropertyValue("--y") - 100 + "px";
+            light.style.left = light.style.getPropertyValue("--x") - 100 + "px";
+
+            lightColors[i].push([]);
+            for (var k = 0; k < lightOnColors.length; ++k) {
+                lightColors[i][j].push(k);
+            }
+            shuffle(lightColors[i][j]);
+
+            lightTimeline[i].push([]);
+            const duration = randomInt(250, 501);
+            lightDuration[i].push(duration);
+            lightTimeline[i][j].push(0);
+            lightTimeline[i][j].push(randomInt(lightTimeline[i][j][0] + 25, duration - 90));
+            lightTimeline[i][j].push(randomInt(lightTimeline[i][j][1] + 25, duration - 60));
+            lightTimeline[i][j].push(randomInt(lightTimeline[i][j][2] + 25, duration - 30));
+
+            light.style.backgroundImage = "radial-gradient(" + lightOffColors[lightColors[i][j][0]] + " " + (60 * size / 100) + "%, transparent " + (70 * size / 100) + "%)";
+            light.style.backgroundPosition = "0 0";
         }
 
-        // temporary solution
-        var button = document.createElement("button");
-        button.innerHTML = "Lights on";
-        const capture = i;
+        for (j = 0; j < nLights; ++j) {
+            tree.append(lights[i][j]);
+        }
+
+        const button = document.createElement("button");
+        button.className = "tree-btn";
+        const captureI = i;
         button.addEventListener("click", () => {
-            for (var l = 0; l < nLights; ++l) {
-                lights[capture][l].lit = 1;
+            animateLight = () => {
+                for (j = 0; j < nLights; ++j) {
+                    var cur = 0;
+                    const frame = lightFrame[captureI] % lightDuration[captureI][j];
+                    while (cur < 3 && frame >= lightTimeline[captureI][j][cur + 1]) {
+                        ++cur;
+                    }
+                    lights[captureI][j].style.backgroundImage = "radial-gradient(" + lerp(lightOnColors[lightColors[captureI][j][cur]], lightOnColors[lightColors[captureI][j][(cur + 1) % 4]], frame - lightTimeline[captureI][j][cur], (lightTimeline[captureI][j][(cur + 1) % 4] - lightTimeline[captureI][j][cur] + lightDuration[captureI][j]) % lightDuration[captureI][j]) + " " + 40 * lights[captureI][j].style.getPropertyValue("--size") / 100 + "%, transparent " + lights[captureI][j].style.getPropertyValue("--size") + "%)";
+                }
+                ++lightFrame[captureI];
             }
+            setInterval(animateLight, 50);
+            registerInteraction();
+            button.disabled = true;
+            button.style.setProperty("--color", "#ffffff00");
         });
-        trees[i].appendChild(button);
         button.style.position = "absolute";
         button.style.top = "100%";
         button.style.left = "33%";
-    }
-};
-
-const showLights = () => {
-    for (var i = 0; i < lights.length; ++i) {
-        for (var j = 0; j < nLights; ++j) {
-            const light = lights[i][j];
-            var gradient = ctx.createRadialGradient(light.x - light.size, light.y - light.size, 0, light.x - light.size, light.y - light.size, light.size);
-            gradient.addColorStop(0.6, lightColors[light.color][light.lit]);
-            gradient.addColorStop(lightRadii[light.lit], "#0c40");
-
-            ctx.beginPath();
-            ctx.arc(light.x - light.size, light.y - light.size, light.size, 0, 2 * Math.PI);
-            ctx.fillStyle = gradient;
-            ctx.fill();
-        }
+        trees[i].appendChild(button);
     }
 };
 
@@ -175,7 +239,6 @@ createLights();
 
 const show = () => {
     ctx.clearRect(0, 0, w, h);
-    showLights();
     updateSnowfall();
 };
 setInterval(show, 50);
@@ -186,5 +249,7 @@ const flameWrap = document.getElementById("flame-wrap");
 const showFire = () => {
     flameWrap.style.animationPlayState = "running";
     flameWrap.style.animationIterationCount = 1;
+    registerInteraction();
+    fireBtn.disabled = true;
 }
 fireBtn.addEventListener("click", showFire);
